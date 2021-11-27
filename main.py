6c1e5887d5760c4d0fd102a26bfc0ab3e3686172
Abhinava Sikdar <51572677+abhisikdar@users.noreply.github.com>
2021-11-26 22:43:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+#import cmdstanpy
+#cmdstanpy.install_cmdstan()
+from cmdstanpy import cmdstan_path, CmdStanModel
+
+stan = 'model2.stan'
+model = CmdStanModel(stan_file=bernoulli_stan)
+model.name
+model.stan_file
+model.exe_file
+model.code()
+
+data = 'data.json'
+variational_vb = model.variational(data=data, output_dir='.',save_diagnostics=True)
